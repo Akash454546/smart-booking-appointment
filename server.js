b7428ff17150c0f450c6bc3c08d2671a36c3c4ea
Akshay C K <akshaycklucky@gmail.com>
2026-03-31 12:06:59 +0530

    made some changes in files

--- server.js
+++ server.js
@@ -30,13 +30,3 @@ app.get('{*splat}', (req, res) => {
 // Connect to MongoDB and start server
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
-  });
