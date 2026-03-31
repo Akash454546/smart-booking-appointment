6a48670aa6d7f376c05a0866357b88091fe1b39f
Akshay C K <akshaycklucky@gmail.com>
2026-03-31 12:06:00 +0530

    made some changes in files

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -33,4 +33,11 @@ function validateLogin(req, res, next) {
   const { email, password } = req.body;
   const errors = [];
 
-  
+  if (!email) errors.push('Email is required');
+  if (!password) errors.push('Password is required');
+
+  if (errors.length) return res.status(400).json({ errors });
+  next();
+}
+
+module.exports = { validateBooking, validateRegistration, validateLogin };
