07ee603987b86d1c95c39e7fda05d87683b7027b
hemanthhr <hemanthhrhemanth8@gmail.com>
2026-03-31 12:43:46 +0530

    made some changes in files

--- server/models/Timeslot.js
+++ server/models/Timeslot.js
@@ -1,9 +1,2 @@
 const mongoose = require('mongoose');
 
-const timeSlotSchema = new mongoose.Schema({
-  doctorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
-  date: { type: String, required: true }, // YYYY-MM-DD
-  slot: { type: String, required: true }, // e.g. "09:00-09:30"
-  isBooked: { type: Boolean, default: false }
-});
-
